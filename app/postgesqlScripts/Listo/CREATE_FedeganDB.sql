b02d8d4cee156b3bb2a9d89115ed2ae586d13e1b
Ovidio Andrade <oviidio.andrade@gmail.com>
2023-02-24 07:54:10 -0500

    DevOAndrade: procesing database

---
+++ app/postgesqlScripts/Listo/CREATE_FedeganDB.sql
@@ -0,0 +1,105 @@
+CREATE TABLE Parametros (
+  idParametro INTEGER PRIMARY KEY,
+  NombreParametro VARCHAR(150),
+  Valor INTEGER,
+  idPadre INTEGER,
+  Descripcion VARCHAR(500)
+);
+
+CREATE TABLE produccionesLecheSacrificio006 (
+  Anio INTEGER,
+  Sacrificio_Bovino_milesxcab DOUBLE PRECISION,
+  Produccion_carne_Ton DOUBLE PRECISION,
+  Tasa_extraccion_porcentage	DOUBLE PRECISION,
+  Produccion_leche_cruda_millonesxlts DOUBLE PRECISION,
+  Acopio_Industrial_millonesxlts DOUBLE PRECISION
+);
+
+CREATE TABLE precioUSDNovilloGordoPie014 (
+  Pais VARCHAR(50),
+  Fecha DATE,
+  Precio DOUBLE PRECISION
+);
+
+CREATE TABLE PrecioLecheCrudaUSDxL018 (
+  Fecha DATE,
+  Argentina DOUBLE PRECISION,
+  Brasil DOUBLE PRECISION,
+  Chile DOUBLE PRECISION,
+  Colombia DOUBLE PRECISION,
+  Estados_Unidos DOUBLE PRECISION,
+  Union_Europea DOUBLE PRECISION,
+  Uruguay DOUBLE PRECISION,
+  Mexico DOUBLE PRECISION,
+  Nueva_Zelanda DOUBLE PRECISION,
+  China DOUBLE PRECISION
+);
+
+CREATE TABLE costosProduccion020 (
+  Indice	INTEGER,
+  Anio	INTEGER,
+  Valor	FLOAT
+);
+
+CREATE TABLE consumosAnualCarne023 (
+  Anio INTEGER,
+  Consumo_proteina_animal	FLOAT,
+  Carne_res_kgxhab FLOAT,
+  Carne_pollo_kgxhab FLOAT,
+  Carne_cerdo_kgxhab FLOAT,
+  Leche_Ltxhab FLOAT,
+  Pescado_kgxhab FLOAT
+);
+
+CREATE TABLE consumosAnualesLeche023 (
+  Anio INTEGER,
+  Leche_Ltxhab DOUBLE PRECISION
+);
+
+CREATE TABLE litroLechePagado046 (
+  Bonificacion INTEGER,
+  Fecha DATE,
+  Precio_por_litro_Nacional DOUBLE PRECISION
+);
+
+CREATE TABLE producionesCarne048 (
+  Anio INTEGER,
+  Toneladas_eq_canal INTEGER
+);
+
+CREATE TABLE ProduccionAcopioLeche050 (
+  Anio INTEGER,
+  Produccion_millonesxlts DOUBLE PRECISION,
+  Acopio_millonesxlts DOUBLE PRECISION
+);
+
+CREATE TABLE sacrificioMensualBovino051 (
+  Fecha DATE,
+  Sacrificio INTEGER
+);
+
+CREATE TABLE hembrasEnSacificio052 (
+  Fecha DATE,
+  Porcentaje_de_hembras FLOAT
+);
+
+CREATE TABLE ganadoGordoEnPie053 (
+  Fecha DATE,
+  Precio_x_kilo DOUBLE PRECISION
+);
+
+CREATE TABLE hembrasFlacaEnpie065 (
+  Fecha DATE,
+  Precio_Region_Caribe INTEGER,
+  Precio_Magdalena_Medio_Santanderes INTEGER,
+  Precio_Llanos_Orientales INTEGER
+);
+
+CREATE TABLE MachoCebaGordoPie071 (
+  Fecha DATE,
+  Precio_Region_Caribe INTEGER,
+  Precio_Magdalena_Medio INTEGER,
+  Precio_Llanos_Orientales INTEGER
+);
+
+
