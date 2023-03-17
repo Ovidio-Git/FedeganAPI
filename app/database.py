60e82693def094a0ddb730ca454a9f1e00e18c58
Ovidio Andrade <oviidio.andrade@gmail.com>
2023-03-17 10:50:38 -0500

    DevOAndrade: change for config database 47

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL="postgresql://root:passwordRoot@db:5432/fedeganDB"
+DATABASE_URL="postgresql://root:passwordRoot@10.5.0.6:5432/fedeganDB"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
